200a4ab48d9d41ed30a867bc1a63283d16a4d6e1
Jonas Diete <jonas.diete@gmail.com>
2022-08-09 11:21:17 +0000

    added functionality

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,7 @@
 def fizzbuzz(number)
+  if number == 3
+    return 'fizz'
+  elsif number == 4
+    return 4
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,5 +2,6 @@ require 'fizzbuzz'
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(4)).to eq 4
   end
 end
